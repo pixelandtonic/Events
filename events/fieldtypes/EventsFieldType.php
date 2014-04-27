24a47271c79d55c796b5ae99b0472c8565c38595
Brandon Kelly <brandon@pixelandtonic.com>
2014-04-27 05:24:49 -0400

    Events field type

---
+++ events/fieldtypes/EventsFieldType.php
@@ -0,0 +1,25 @@
+<?php
+namespace Craft;
+
+/**
+ * Entries field type
+ */
+class EventsFieldType extends BaseElementFieldType
+{
+	/**
+	 * @access protected
+	 * @var string $elementType The element type this field deals with.
+	 */
+	protected $elementType = 'Events_Event';
+
+	/**
+	 * Returns the label for the "Add" button.
+	 *
+	 * @access protected
+	 * @return string
+	 */
+	protected function getAddButtonLabel()
+	{
+		return Craft::t('Add an event');
+	}
+}
